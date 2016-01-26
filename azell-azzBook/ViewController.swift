13e9a5c074a0905b6e4e37937f93d2f081d06073
Marcel Canhisares <marcelcanhisares@macbook-air-de-karla.local>
2016-01-26 15:24:46 -0200

    Initial Commit

---
+++ azell-azzBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  azell-azzBook
+//
+//  Created by Marcel Canhisares on 26/01/16.
+//  Copyright © 2016 Azell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
